d4cf44f1b773b720a838f2b5297143bf23509e72
Alice Njorge <alicenjoroge707@gmail.com>
2019-08-25 09:24:58 +0300

    add assignable fields for Schema attribute

--- app/SchemaAttribute.php
+++ app/SchemaAttribute.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class SchemaAttribute extends Model
 {
-    //
+    /**
+     * mass assignable field
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'type',
+        'size',
+        'null',
+        'index',
+        'primary_key',
+        'schema_id'
+    ];
 }
