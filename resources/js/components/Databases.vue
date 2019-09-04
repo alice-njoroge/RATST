8347ff848901c79171d27f3d164e45f338a85f36
Alice <alicenjoroge707@gmail.com>
2019-09-04 09:32:53 +0300

    add  on comments on the component

--- resources/js/components/Databases.vue
+++ resources/js/components/Databases.vue
@@ -3,6 +3,7 @@
         <p v-for="item in database_list"><a :href="'/parser/' + item.database">{{item.database}}</a></p>
     </div>
 </template>
+// functionality to fetch data from inside otherwise use props
 <script>
     export default {
         name: 'Databases',
@@ -11,12 +12,14 @@
                 database_list: []
             }
         },
+        // http client axios to access urls
         created() {
             axios({
                 'method': 'get',
                 'url': '/databases'
             })
-                .then(res => this.database_list = res.data)
+                // asynchronuous processing working on promises to return data
+                .then(res => this.database_list = res.data) // funtion takes the response as an argument and returns in in the internal variable database_list
                 .catch(function (error) {
                     console.log(error);
                 })
