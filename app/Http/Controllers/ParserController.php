f40a2c601e657583b5b072affeab731ce15d3877
Alice Njorge <alicenjoroge707@gmail.com>
2019-11-16 13:58:42 +0300

    rename columns

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -37,6 +37,46 @@ class ParserController extends Controller
         return $pdo;
     }
 
+    private function rename_operation(Array $result, string $database_name)
+    {
+        if (array_key_exists('new_relation_name', $result)) {
+        } else {
+            $table_name = $result['relation_name'];
+            $new_column_name = $result['new_attribute_name'];
+            $old_column_name = $result['old_attribute_name'];
+            $pdo = $this->get_pdo($database_name);
+            $stmt = $pdo->query("show columns from " . $table_name);
+            $columns = $stmt->fetchAll();
+            $key = array_search($old_column_name, array_column($columns, 'Field'));
+            $data_type = $columns[$key]['Type'];
+            //Create sql column update statement
+            $update_sql = "alter table {$table_name} change {$old_column_name} {$new_column_name} {$data_type}";
+            $pdo->exec($update_sql);
+            $database_results = [
+                [
+                    'Key' => 'Table',
+
+                    'Value' => $table_name
+                ],
+                [
+                    'Key' => 'Old Column Name',
+
+                    'Value' => $old_column_name
+                ],
+                [
+                    'Key' => 'New  Column Name',
+
+                    'Value' => $new_column_name
+                ],
+            ];
+            return [
+                'database_results' => $database_results,
+                'sql_out' => $update_sql . ';'
+            ];
+        }
+        return [];
+    }
+
     /**
      * @param Request $request
      * @return JsonResponse
@@ -48,6 +88,7 @@ class ParserController extends Controller
             'database_name' => 'required',
             'query' => 'required'
         ]);
+        $database_name = $request->input('database_name');
         //initialize new  guzzle client with url
         $client = new Client([
             'base_uri' => 'localhost:5000',
@@ -56,12 +97,19 @@ class ParserController extends Controller
             'query' => ['query' => $request->input('query')]
         ]);
         $result = json_decode((string)$response->getBody(), true);
-        $pdo = $this->get_pdo($request->input('database_name'));
-        $stmt = $pdo->query($result['result']); // run the query against the initialized pdo
-        $database_results = $stmt->fetchAll(); // fetch db results
+        if (gettype($result['result']) == 'array') {
+            $result = $this->rename_operation($result['result'], $database_name);
+            $database_results = $result['database_results'];
+            $sql_output = $result['sql_out'];
+        } else {
+            $pdo = $this->get_pdo($database_name);
+            $stmt = $pdo->query($result['result']); // run the query against the initialized pdo
+            $database_results = $stmt->fetchAll(); // fetch db results
+            $sql_output = $result['result'] . ";";
+        }
         $results = view('pages.parser.tabular-results', [
             'database_results' => $database_results,
-            'sql_output' => $result['result'].";"
+            'sql_output' => $sql_output
         ])->render();
         return response()->json($results);
     }
