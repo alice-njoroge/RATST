eefe17685814dc9aa605f05a1535a5def1e75c65
alice-njoroge <alicenjoroge707@gmail.com>
2019-11-27 10:32:16 +0300

    update index blade on results card

--- resources/views/pages/help/about-the-developer.blade.php
+++ resources/views/pages/help/about-the-developer.blade.php
@@ -3,25 +3,30 @@
     <main role="main" class="">
         <div class="card">
             <div class="card-header">
-                <h2>About the developer</h2>
+                <h2>About</h2>
             </div>
             <div class="card-body">
                 <article class="row single-post mt-5 no-gutters">
                     <div>
                         <div class="image-wrapper float-left pr-3">
-                            <img src="https://www.w3schools.com/bootstrap4/cinqueterre.jpg" class="rounded"
-                                 alt="Cinque Terre" width="304" height="236">
+                            <img src="{{asset('images/alice.jpg')}}" class="rounded"
+                                 alt="Cinque Terre" width="210" height="236">
                         </div>
                         <div class="single-post-content-wrapper p-3">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
+
+                            <p><b>Name:</b> Alice Njoroge</p>
+                            <p><b>University:</b> Dedan Kimathi University</p>
+                            <p><b>Course:</b> Bsc. Computer Science </p>
+                            <p><b>Skills:</b> Laravel and Vue Js</p>
+                            <p><b>Project Supervisor:</b> Mr. Kagiri</p>
+
+
                             <br><br>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad, ex eaque fuga minus reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum enim soluta omnis debitis quam ab nemo necessitatibus.
+                             This software is a DLO and a Parser that translates Relational Algebra to equivalent and
+                            valid SQL queries. It is meant to ease the learning curve of foundational concepts to Database students.
+                            <br>Many Thanks to My supervisor Mr. Kagiri, without him this would be
+                            completely impossible!!
+
                         </div>
                     </div>
                 </article>
